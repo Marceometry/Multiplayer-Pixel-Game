05322f49735890b0a29d46eea8d42bc34d8ca6f3
Marcelino <marceometry@gmail.com>
2021-08-16 10:31:40 -0300

    add changes to heroku deploy

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.get('/admin-options', function (req, res) {
     res.sendFile(`${process.cwd()}/public/game-admin.html`)
 })
 
-server.listen(8000, () => {
-    console.log('> Server listening on port 8000 (http://localhost:8000)')
-})
+server.listen(process.env.PORT || 8000)
 
 
 // game //
